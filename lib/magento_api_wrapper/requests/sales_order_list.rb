df03564d538dd8a5a2449843f93cc727645a1163
Josh Harris <josh@shippingeasy.com>
2014-02-06 10:59:12 -0500

    WIP: Configurable filters for sales_order_list, make all filters optional

--- lib/magento_api_wrapper/requests/sales_order_list.rb
+++ lib/magento_api_wrapper/requests/sales_order_list.rb
@@ -8,38 +8,50 @@ module MagentoApiWrapper::Requests
     end
 
     def body
-      sales_order_list_hash
+      merge_filters!(sales_order_list_hash)
     end
 
     def sales_order_list_hash
       {
-        session_id: self.session_id,
-        filters: {
-          "complex_filter" => filters_array
-        }
+        session_id: self.session_id
       }
+    end
+
+    def merge_filters!(sales_order_list_hash)
+      sales_order_list_filters = {}
+      if !filters_array.empty?
+        sales_order_list_filters["complex_filter"] = filters_array
+        sales_order_list_hash.merge!(sales_order_list_filters)
+      else
+        sales_order_list_hash
+      end
     end
 
     def filters_array
-      filters = {}
-      filters.compare_by_identity
-      filters["complexObjectArray"] = {
-        key: "created_at",
-        value: created_at_hash
-      }
-      filters["complexObjectArray"] = {
-        key: "state",
-        value: status_hash
-      }
-      filters
+      custom_filters = {}
+      custom_filters.compare_by_identity
+      if last_modified
+        custom_filters["complexObjectArray"] = {
+          key: "created_at",
+          value: created_at_hash
+        }
+      end
+      if status_array
+        custom_filters["complexObjectArray"] = {
+          key: "state",
+          value: status_hash
+        }
+      end
+      if filters
+        filters.each do |key, value_hash|
+          custom_filters["complexObjectArray"] = {
+            key: key,
+            value: value_hash
+          }
+        end
+      end
+      custom_filters
     end
-
-      #{complex_object_array: ,
-            #complex_object_array: {
-              #key: "created_at",
-              #value: created_at_hash
-            #}
-      #}
 
     def status_hash
       status = {}
@@ -62,9 +74,20 @@ module MagentoApiWrapper::Requests
       created_from
     end
 
+    def created_at_key
+      data[:created_at_key]
+    end
+
+    def last_modified
+      data[:last_modified]
+    end
+
     def date_filter
-      date_filter = data[:last_modified] ? Time.parse(data[:last_modified]).beginning_of_day.to_formatted_s(:db) : (Time.now - 2.weeks).beginning_of_day.to_formatted_s(:db)
-      return date_filter
+      begin
+        Time.parse(last_modified).beginning_of_day.to_formatted_s(:db)
+      rescue MagentoApiWrapper::BadRequest => e
+        raise e
+      end
     end
 
     def session_id
